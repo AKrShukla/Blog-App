a3799a14fb6ed2e24d95e9fc7d1ab5281ed65f4e
Anurag Kumar Shukla <angshkl@gmail.com>
2018-11-15 22:13:49 +0530

    Improved index.html

--- blog/urls.py
+++ blog/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('about/',views.about,name = "about"),
     path('articles/',include("article.urls")),
     path('user/',include("user.urls")),
+    path('contact/',views.contact,name = "contact"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
